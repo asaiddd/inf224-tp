b5e2d0183f1a5f05aeb42e7ddf6f392fd721e8ce
Ahmet Sait Tekkurt <sait.tekkurt@tradewaretech.com>
2020-11-11 11:50:14 +0300

    tp03 files are added to repo

---
+++ tp03/splay-tree.h
@@ -0,0 +1,5 @@
+#ifndef __SPLAY_TREE_H__
+#define __SPLAY_TREE_H__
+
+
+#endif /* __SPLAY_TREE_H__ */
---
+++ tp03/test.c
@@ -0,0 +1,9 @@
+/*  Name : A.Sait Tekkurt
+    Date : 11.11.2020   */
+
+
+int main(void)
+{
+
+    return 0;
+}
